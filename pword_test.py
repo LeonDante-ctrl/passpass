3be4dd77f04d8ece5d8374fdb3ce635e4cd86f65
dante <maxgamerdu@gmail.com>
2022-01-23 10:06:56 +0300

    added a test class to confirm if user can save in the users list as well as tested for credentials functionality

--- pword_test.py
+++ pword_test.py
@@ -19,5 +19,29 @@ class UserTest(unittest.TestCase):
         self.assertEqual(self.new_user.username,'Danteiusimus')
         self.assertEqual(self.new_user.password,'yernannylovesmeballs')
 
+     def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
+        self.new_credential = Credentials('Gmail','LeonDante','4fhGifd74')
+    def test_init(self):
+        """
+        check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'Gmail')
+        self.assertEqual(self.new_credential.userName,'Leon_Dante')
+        self.assertEqual(self.new_credential.password,'fsdjis73')    
+
 if __name__ == "__main__":
     unittest.main()
